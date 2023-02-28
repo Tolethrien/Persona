af33b58539f7fc97154d866f4f9bfe1118c83f02
Paweł Grela <tolethrien@gmail.com>
2023-02-28 18:24:14 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: { port: 3000, open: true },
+  },
+});
