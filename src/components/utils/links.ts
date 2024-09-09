dcfacaa6afa837ac2eaa313c5568dea158cf7605
Pavel <tolethrien@gmail.com>
2024-09-09 20:42:04 +0200

    image next and prev buttons

--- src/components/utils/links.ts
+++ src/components/utils/links.ts
@@ -1,4 +1,4 @@
 export const LINK_GITHUB = "https://github.com/Tolethrien";
 export const LINK_LINKEDIN =
   "https://www.linkedin.com/in/paweł-grela-762165234/";
-export const EMAIL_ADRESS = "pawe@gma.co";
+export const EMAIL_ADRESS = "emberbornstudios@gmail.com";
